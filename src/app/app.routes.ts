75beb9694d3e3cb7f2e909807e665ccb4e47fcad
Femi Omage <omagebi@yahoo.com>
2024-01-17 06:37:17 +0100

    sss

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,11 +2,17 @@ import { Routes } from '@angular/router';
 import { PhotoComponent } from './Gallery/photo/photo.component';
 import { PhotoListComponent } from './Gallery/photo-list/photo-list.component';
 import { HomeComponent } from './home/home.component';
+import { GalleryComponent } from './Gallery/gallery.component';
 
 export const routes: Routes = [
+  // { path: '', redirectTo: 'image/upload', pathMatch: 'full' },
   { path: '', redirectTo: 'home', pathMatch: 'full' },
   { path: 'home', component: HomeComponent },
-  { path: 'uploadimages', component: PhotoComponent },
-  { path: 'viewimages', component: PhotoListComponent },
+  {
+    path: 'image', component: GalleryComponent, children: [
+      { path: 'upload', component: PhotoComponent },
+      { path: 'list', component: PhotoListComponent }
+    ]
+  },
   { path: '**', redirectTo: 'home', pathMatch: 'full' }
 ];
